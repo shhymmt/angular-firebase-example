7c2e04435474f796ad27100525701c261a348755
shhymmt <mail@shhymmt.com>
2020-09-28 14:41:29 +0900

    feat(home):petインターフェイス作成

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
+import { PetComponent } from './pet/pet.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, PetComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
