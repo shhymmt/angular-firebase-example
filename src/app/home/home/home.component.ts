aa67d5c81f3863b366e53609dc36558c9f980071
shhymmt <mail@shhymmt.com>
2020-09-28 16:44:21 +0900

    feat(home):コンポーネントで値を受け取る

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+
+  pet: Pet = {
+    name: 'テストペット',
+    exp: 200,
+    level: 4,
+    avatarURL: '/assets/images/pet-1.png'
+  };
+
   constructor() { }
 
   ngOnInit(): void {
